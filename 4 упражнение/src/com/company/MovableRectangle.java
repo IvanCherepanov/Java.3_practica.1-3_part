371467a395a8afee3ae79f5229b12cecf8966bde
Ivan <iv.cherepanov@yandex.ru>
2021-09-06 01:28:25 +0300

    5 part done

---
+++ 4 упражнение/src/com/company/MovableRectangle.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class MovableRectangle implements Movable{
+    private MovablePoint topLeft;
+    private MovablePoint bottomRight;
+
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xspeed, int yspeed) {
+        this.topLeft = new MovablePoint(x1,y1,xspeed,yspeed);
+        this.bottomRight = new MovablePoint(x2,y2,xspeed,yspeed);
+    }
+
+    @Override
+    public String toString() {
+        return "MovableRectangle{" +
+                "topLeft=" + topLeft +
+                ", bottomRight=" + bottomRight +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
