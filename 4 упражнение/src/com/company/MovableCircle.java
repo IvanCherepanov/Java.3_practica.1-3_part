6ff35e671d53bd0c1c9aeae0a4ec97cd2c161a4b
Ivan <iv.cherepanov@yandex.ru>
2021-09-06 01:17:11 +0300

    4 part done

--- 4 упражнение/src/com/company/MovableCircle.java
+++ 4 упражнение/src/com/company/MovableCircle.java
@@ -1,4 +1,31 @@
 package com.company;
 
-public class MovableCircle {
+public class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int radius, int x, int y, int xspeed, int yspeed) {
+        this.radius = radius;
+        this.center = new MovablePoint(x,y,xspeed,yspeed);
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
 }
